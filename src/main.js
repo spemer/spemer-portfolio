3aafeff037a3ede96ca46e60e2d4ecec5db5b306
spemer <ghsspower@naver.com>
2018-08-05 00:31:40 +0900

    init

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import firebase from 'firebase'
 import VueFire from 'vuefire'
 
 
 Vue.use(VueFire)
-
-
 
 
 new Vue({
