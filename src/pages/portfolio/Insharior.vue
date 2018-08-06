57d6ff305804fc25d8c98f1c789a8db98c119917
spemer <ghsspower@naver.com>
2018-08-06 14:35:48 +0900

    init

--- src/pages/portfolio/Insharior.vue
+++ src/pages/portfolio/Insharior.vue
@@ -28,6 +28,53 @@
 
       div.page__section
         h3.section__title How to solve
+        div.section__list
+          ol
+            li Not only provide users know-how about self interior, but also provide platform to 'share' their furniture with trust so they can earn money with that or get unique interior items from other users
+            li Integrate with social network services like Facebook and Instagram - enable upload image of their room via SNS to keep DAU(Insharior's main target is young people in South Korea, especially one-person house holds)
+            li Authentication via SNS and phone number can improve trust between users
+            li To provide accurate and various information, collect and analyze user's taste from their search patterns or tutorial - with data from such processes, show customized information and products on our chat bot or main screen
+            li Provide augmented reality feature to place furniture in user's room in advance before they rent it
+
+      div.page__section
+        h3.section__title #1. Observation
+        p.section__title--small Audience
+        p.section__paragraph Insharior’s main targets are young people in society and early 20’s in South Korea. Due to their economic and occupational conditions, move their house in short term(between 1 ~ 2 years) is very common. It’s the biggest reason why they can’t collect interior items in their house, or buy an expensive housewares.
+
+        p.section__title--small Audience — Lifestyle
+        div.section__list
+          ol
+            li Usually spend their time in small rented room
+            li Desire to Self-disclosure on SNS platforms
+            li Short-term living in their rented room(usually between 1 ~ 2 years)
+            li Usually get information about interior from SNS or TV
+            li Proper investment is available for their house interior
+
+        p.section__title--small Persona
+        p.section__paragraph We created some persona to ascertain our concept and make our plans backed up by examples with other characters. Image below is an example of Insharior’s potential user. Watch more persona on Google Drive(in Korean) - Persona 1,2 | Persona 3
+
+        p.section__title--small What they say
+        div.section__list
+          ul
+            li It's hard to decorate my room due to frequent move. It's kind of a heavy economic burden.
+            li I do not have much money, but I want to decorate my own room well as other do I can see on Instagram.
+            li It's too wasteful and bothersome to buy a new interior item every time I move to another rent room.
+
+      div.page__section
+        h3.section__title #2. Ideation
+        p.section__quote What if we can get a luxury interior item looks like displayed in a department store, with low price like we can get it in a convenience store?
+
+        p.section__title--small From Audience
+        p.section__paragraph Based on our audience above, we set 3 main concepts of Insharior. We wanted to make a platform that provides users can:
+        div.section__list
+          ol
+            li Make a profit with their unused interior items
+            li Rent a unique interior items with low price from other users
+            li Integrate with other SNS application to share their interior
+
+        p.section__title--small Platform
+        p.section__paragraph Basically, Insharior is a ‘sharing economy’ application. Users of Insharior share their interior items on Insharior. We provide this platform with clean and intuitive interface and experience. Integrate it with other SNS like Facebook, Instagram and Twitter is available. We’re going to customize the user-front view, by using collected data from chat-bot and big data. Sharing economy applications like AirBnb, Uber, Lyft and SolarCity have a great ripple effects. They don’t have their own contents, only provide a platform for users, but makes world-class profits.
+        p.section__quote “The share economy blows up the industrial model of companies owning and people consuming.” — Forbes, Jan.
 </template>
 
 <script>
