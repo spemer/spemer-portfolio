38db73505bf19564043621b27889f342b878dbef
spemer <ghsspower@naver.com>
2018-08-06 14:02:22 +0900

    init

--- src/pages/portfolio/Insharior.vue
+++ src/pages/portfolio/Insharior.vue
@@ -11,6 +11,23 @@
 
       div.page__section
         h3.section__title Problems
+        div.section__list
+          ol
+            li A sharp increase of young one-person households(early 20’s)— and their desire to decorate their own room with unique interior items getting higher day by day.
+            li Increase of move due to one-person household’s economic conditions— which makes them hard to move their heavy furniture like bed or couch, or hard to collect interior items.
+            li Increase of desire to self-interior between one-person households and economic weaker— even if they have to move their residence often.
+        p.section__paragraph Based on those problems above, our team decided to make an application that resolves the needs by providing a platform to share one’s interior items with trust at a low price — Insharior.
+
+      div.page__section
+        h3.section__title Challenge - Threats
+        div.section__list
+          ol
+            li There are already many apps for self-interior on App Store / Google Play Store in South Korea
+            li Companies like IKEA, Hanssem(Korean furniture company) provide cheap furniture through offline
+            li Lack of expertise and trust - biggest problem of sharing economics, users can easily have doubt about lenders or item's quality
+
+      div.page__section
+        h3.section__title How to solve
 </template>
 
 <script>
