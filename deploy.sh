c3c69725e74578431f4f742f8e00b227d8054c73
spemer <ghsspower@gmail.com>
2019-03-25 01:14:10 +0900

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "=============================="
+echo "${PWD##*/}"
+echo "=============================="
+
+# run gulpfile.js image resizer
+trap 'echo Stop gulp-watch' SIGINT
+sudo gulp watch
+trap SIGINT
+
+# serve with hot reload at localhost:3000
+npm run dev
+
+# build for production with minification
+npm run build
+
+# deploy with firebase cli
+# firebase deploy
+
+# get commit message
+printf "\n"
+IFS= read -r -p "Enter commit message: " commitmsg
+
+# if commitmsg empty
+if [ -z "$commitmsg" ]
+then
+    echo "Commit message is empty"
+    commitmsg="Add files via upload"
+fi
+
+printf "\n"
+git add .
+git commit -m "$commitmsg"
+git push
+
+exit
